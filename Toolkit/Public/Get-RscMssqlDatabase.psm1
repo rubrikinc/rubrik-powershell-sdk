e0b162684f2482f75d86825c45989076d8078e9c
Chris Lumnah <chris@lumnah.us>
2023-09-21 20:12:12 -0400

    Fixed help

--- Toolkit/Public/Get-RscMssqlDatabase.psm1
+++ Toolkit/Public/Get-RscMssqlDatabase.psm1
@@ -26,11 +26,6 @@ function Get-RscMssqlDatabase {
     Get-RscMssqlDatabase -Name master -InstanceID "f2c15060-85a5-54c4-95e7-7c4a948a3e19"
 
     Return the master database on a specific SQL Server Instance. InstanceId can be retrieved by using Get-RscMssqlInstance
-
-    # .EXAMPLE
-    # Get-RscMssqlDatabaseCRL -name master -InstanceID "f2c15060-85a5-54c4-95e7-7c4a948a3e19" -clusterId "39b92c18-d897-4b55-a7f9-17ff178616d0"
-
-    Return the master database on a specific SQL Server Instance. InstanceId can be retrieved by using Get-RscMssqlInstance, and clusterId can be retrieved by using Get-RscCluster
     #>
 
     [CmdletBinding(DefaultParameterSetName = "List")]
