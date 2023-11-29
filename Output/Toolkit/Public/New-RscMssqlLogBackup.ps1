76734c9c60452c26edd6ed003761260ffdc89aa3
Chris Lumnah <chris@lumnah.us>
2023-11-28 20:24:27 -0500

    Live Mount, Restore, and Log Backup

--- Output/Toolkit/Public/New-RscMssqlLogBackup.ps1
+++ Output/Toolkit/Public/New-RscMssqlLogBackup.ps1
@@ -14,9 +14,6 @@ function New-RscMssqlLogBackup {
     .PARAMETER RscMssqlDatabase
     Database object returned from Get-RscMssqlDatabase
     
-    .PARAMETER AsQuery
-    Instead of running the command, the query and variables used for the query will be returned. 
-    
     .EXAMPLE
     Returns the list of database files based on the latest recovery point
     $RscMssqlDatabase = Get-RscMssqlDatabase -Name AdventureWorks2019
@@ -30,29 +27,16 @@ function New-RscMssqlLogBackup {
             Mandatory = $false, 
             ValueFromPipeline = $true
         )]
-        [RubrikSecurityCloud.Types.MssqlDatabase]$RscMssqlDatabase,
-
-        #  Common parameter to all parameter sets:
-        [Parameter(
-            Mandatory = $false, 
-            ValueFromPipeline = $false
-        )][Switch]$AsQuery
+        [RubrikSecurityCloud.Types.MssqlDatabase]$RscMssqlDatabase
     )
     Process {
-        # Re-use existing connection, or create a new one:
-        Connect-Rsc -ErrorAction Stop | Out-Null
-
         #region Create Query
         $query = New-RscMutationMssql -Operation TakeLogBackup
         $query.Var.input = New-Object -TypeName RubrikSecurityCloud.Types.TakeMssqlLogBackupInput
         $query.Var.input.id = "$($RscMssqlDatabase.Id)"
         #endregion
         
-        if ( $AsQuery -eq $true ) {
-            $result = $query.GqlRequest()
-        }else{
-            $result = $query.Invoke()
-        }
+        $result = $query.Invoke()
         $result
     } 
 }
