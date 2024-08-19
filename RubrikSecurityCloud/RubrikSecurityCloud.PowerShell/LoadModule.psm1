9a8588d67ddf010210bff6f8394f8e8eb0f32133
Rynardt Spies <rynardt.spies@rubrik.com>
2024-08-19 16:40:30 +0100

    Remove console logging for some tests to pass. Redirect System.ComponentModel.Annotations.

--- RubrikSecurityCloud/RubrikSecurityCloud.PowerShell/LoadModule.psm1
+++ RubrikSecurityCloud/RubrikSecurityCloud.PowerShell/LoadModule.psm1
@@ -8,31 +8,36 @@ try {
 
     # Determine the module directory based on the PowerShell edition
     If ($PSVersionTable.PSEdition -eq "Desktop") {
-        Write-Host "`nLoading Rubrik Security Cloud PowerShell Module (WindowsPowerShell)...`n"
+       # Write-Host "`nLoading Rubrik Security Cloud PowerShell Module (WindowsPowerShell)...`n"
         $moduleDir = Join-Path -Path $PSScriptRoot -ChildPath "net461"
 
         # Load the specific versions of required assemblies
         $unsafeAssemblyPath = Join-Path -Path $moduleDir -ChildPath 'System.Runtime.CompilerServices.Unsafe.dll'
         $vectorsAssemblyPath = Join-Path -Path $moduleDir -ChildPath 'System.Numerics.Vectors.dll'
         $buffersAssemblyPath = Join-Path -Path $moduleDir -ChildPath 'System.Buffers.dll'
+        $annotationsAssemblyPath = Join-Path -Path $moduleDir -ChildPath 'System.ComponentModel.Annotations.dll'
         
         # Force loading the correct assembly versions
-        $loadedUnsafeAssembly = [System.Reflection.Assembly]::LoadFrom($unsafeAssemblyPath)
+        # [System.Reflection.Assembly]::LoadFrom($unsafeAssemblyPath)
 
         # Add the AssemblyResolve event handler to persist throughout the session
         $onAssemblyResolve = [System.ResolveEventHandler] {
             param($sender, $e)
             if ($e.Name -like 'System.Runtime.CompilerServices.Unsafe, *') {
-               # Write-Host "LoadModule.psm1: Redirecting $e.Name to version 6.0.0.0 via AssemblyResolve event handler"
+                # Write-Host "LoadModule.psm1: Redirecting $e.Name to version 6.0.0.0 via AssemblyResolve event handler"
                 return [System.Reflection.Assembly]::LoadFrom($unsafeAssemblyPath)
             }
             elseif ($e.Name -like 'System.Numerics.Vectors, *') {
-               # Write-Host "LoadModule.psm1: Redirecting $e.Name to the correct System.Numerics.Vectors assembly"
+                # Write-Host "LoadModule.psm1: Redirecting $e.Name to the correct System.Numerics.Vectors assembly"
                 return [System.Reflection.Assembly]::LoadFrom($vectorsAssemblyPath)
             }
             elseif ($e.Name -like 'System.Buffers, *') {
-               # Write-Host "LoadModule.psm1: Redirecting $e.Name to the correct System.Buffers assembly"
+                # Write-Host "LoadModule.psm1: Redirecting $e.Name to the correct System.Buffers assembly"
                 return [System.Reflection.Assembly]::LoadFrom($buffersAssemblyPath)
+            }
+            elseif ($e.Name -like 'System.ComponentModel.Annotations, *') {
+                # Write-Host "LoadModule.psm1: Redirecting $e.Name to the correct System.ComponentModel.Annotations assembly"
+                return [System.Reflection.Assembly]::LoadFrom($annotationsAssemblyPath)
             }
         }
 
@@ -40,7 +45,7 @@ try {
         [System.AppDomain]::CurrentDomain.add_AssemblyResolve($onAssemblyResolve)
     }
     else {
-        Write-Host "`nLoading Rubrik Security Cloud PowerShell Module (PowerShell Core)...`n"
+      # Write-Host "`nLoading Rubrik Security Cloud PowerShell Module (PowerShell Core)...`n"
         $moduleDir = Join-Path -Path $PSScriptRoot -ChildPath "net6.0"
     }
 
@@ -48,12 +53,12 @@ try {
     Set-Location -Path $moduleDir
     $modulePath = Join-Path -Path $moduleDir -ChildPath "RubrikSecurityCloud.PowerShell.dll"
     
-    Write-Host "LoadModule.psm1: Import-Module $modulePath"
+    # Write-Host "LoadModule.psm1: Import-Module $modulePath"
     Import-Module $modulePath -ErrorAction Stop
 
-    Write-Host "Welcome to the Rubrik Security Cloud PowerShell SDK!`n"
-    Write-Host "`tPlease refer to the Getting Started section of the Readme documentation: "
-    Write-Host "`thttps://github.com/rubrikinc/rubrik-powershell-sdk#getting-started`n"
+   # Write-Host "Welcome to the Rubrik Security Cloud PowerShell SDK!`n"
+   # Write-Host "`tPlease refer to the Getting Started section of the Readme documentation: "
+   # Write-Host "`thttps://github.com/rubrikinc/rubrik-powershell-sdk#getting-started`n"
 }
 catch {
     Write-Error "Unable to load Rubrik Security Cloud Module: $_"
