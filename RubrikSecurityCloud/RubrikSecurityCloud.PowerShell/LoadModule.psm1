56e50c64304a338694c7fffd748f941a092179f6
Rynardt Spies <rynardtspies@users.noreply.github.com>
2024-08-22 17:13:19 +0100

    Fix assembly conflict in Windows PowerShell 5.1 (#132)
    * utils scripts
    
    * utils scripts
    
    * utils scripts
    
    * Update CHANGELOG.md
    
    * Create dotnet.yml
    
    * run Utils/Build-RscSdk.ps1
    
    * docs update
    
    * schema update
    
    * schema update
    
    * build scripts
    
    * fix for ubuntu vm build (github action)
    
    * Update and rename dotnet.yml to build.yml
    
    * add manual build ability
    
    * add SA file as action secret
    
    * Ensure PowerShell config directory exists
    
    * CI config for tests
    
    * toolkit script
    
    * CI config for tests
    
    * run in CI mode
    
    * testing CI (4)
    
    * testing CI (5)
    
    * utils
    
    * SA unavailable exception
    
    * SA unavailable exception
    
    * remove build artifact from main branch
    
    * doc update
    
    * schema update
    
    * Operations now part of toolkit
    
    * release 0.29
    
    * build scripts update
    
    * build scripts update
    
    * build scripts
    
    * Add support for subfield arguments (handwritten changes) (#65)
    
    * Prepare for next development iteration
    
    * MSSQL cmdlet Updates (#63)
    
    * Now works with Host/FCI/ID/List
    
    * Allow for filtering by InstanceName
    
    * Correct bugs on GetInstance and added ID to GetDatabase
    
    * Updates to MSSQL
    
    * Update to PR
    
    * change log updates
    
    * Update TOOLKIT_DEVELOPER_MANUAL.md about operations
    
    * Expand interfaces in -AddField and -RemoveField
    
    * Patched clusterConnection DETAIL profile
    
    * Add support for subfield arguments (handwritten changes)
    
    ---------
    
    Co-authored-by: Gui <12204241+guirava@users.noreply.github.com>
    Co-authored-by: Chris Lumnah <serif_jibes02@icloud.com>
    
    * Add support for subfield arguments (autogenerated changes) (#66)
    
    * Prepare for next development iteration
    
    * MSSQL cmdlet Updates (#63)
    
    * Now works with Host/FCI/ID/List
    
    * Allow for filtering by InstanceName
    
    * Correct bugs on GetInstance and added ID to GetDatabase
    
    * Updates to MSSQL
    
    * Update to PR
    
    * change log updates
    
    * Update TOOLKIT_DEVELOPER_MANUAL.md about operations
    
    * Expand interfaces in -AddField and -RemoveField
    
    * Patched clusterConnection DETAIL profile
    
    * Add support for subfield arguments (handwritten changes)
    
    * Add support for subfield arguments (autogenerated changes)
    
    ---------
    
    Co-authored-by: Gui <12204241+guirava@users.noreply.github.com>
    Co-authored-by: Chris Lumnah <serif_jibes02@icloud.com>
    
    * commit fix
    
    * commit fix
    
    * Update packages. Redirect assembly loading to resolve library conflicts
    
    * Remove console logging for some tests to pass. Redirect System.ComponentModel.Annotations.
    
    ---------
    
    Co-authored-by: Gui <12204241+guirava@users.noreply.github.com>
    Co-authored-by: evcheng-rubrik <97479644+evcheng-rubrik@users.noreply.github.com>
    Co-authored-by: Chris Lumnah <serif_jibes02@icloud.com>
    Co-authored-by: Evan Cheng <evan.cheng@rubrik.com>

--- RubrikSecurityCloud/RubrikSecurityCloud.PowerShell/LoadModule.psm1
+++ RubrikSecurityCloud/RubrikSecurityCloud.PowerShell/LoadModule.psm1
@@ -6,29 +6,62 @@ try {
     # are done loading the module
     $currentPath = Get-Location
 
+    # Determine the module directory based on the PowerShell edition
     If ($PSVersionTable.PSEdition -eq "Desktop") {
-        Write-Output("`nLoading Rubrik Security Cloud PowerShell Module (WindowsPowerShell)...`n")
-        $moduleDir = Join-Path -Path $PSScriptRoot -ChildPath "net472"
+       # Write-Host "`nLoading Rubrik Security Cloud PowerShell Module (WindowsPowerShell)...`n"
+        $moduleDir = Join-Path -Path $PSScriptRoot -ChildPath "net461"
+
+        # Load the specific versions of required assemblies
+        $unsafeAssemblyPath = Join-Path -Path $moduleDir -ChildPath 'System.Runtime.CompilerServices.Unsafe.dll'
+        $vectorsAssemblyPath = Join-Path -Path $moduleDir -ChildPath 'System.Numerics.Vectors.dll'
+        $buffersAssemblyPath = Join-Path -Path $moduleDir -ChildPath 'System.Buffers.dll'
+        $annotationsAssemblyPath = Join-Path -Path $moduleDir -ChildPath 'System.ComponentModel.Annotations.dll'
+        
+        # Force loading the correct assembly versions
+        # [System.Reflection.Assembly]::LoadFrom($unsafeAssemblyPath)
+
+        # Add the AssemblyResolve event handler to persist throughout the session
+        $onAssemblyResolve = [System.ResolveEventHandler] {
+            param($sender, $e)
+            if ($e.Name -like 'System.Runtime.CompilerServices.Unsafe, *') {
+                # Write-Host "LoadModule.psm1: Redirecting $e.Name to version 6.0.0.0 via AssemblyResolve event handler"
+                return [System.Reflection.Assembly]::LoadFrom($unsafeAssemblyPath)
+            }
+            elseif ($e.Name -like 'System.Numerics.Vectors, *') {
+                # Write-Host "LoadModule.psm1: Redirecting $e.Name to the correct System.Numerics.Vectors assembly"
+                return [System.Reflection.Assembly]::LoadFrom($vectorsAssemblyPath)
+            }
+            elseif ($e.Name -like 'System.Buffers, *') {
+                # Write-Host "LoadModule.psm1: Redirecting $e.Name to the correct System.Buffers assembly"
+                return [System.Reflection.Assembly]::LoadFrom($buffersAssemblyPath)
+            }
+            elseif ($e.Name -like 'System.ComponentModel.Annotations, *') {
+                # Write-Host "LoadModule.psm1: Redirecting $e.Name to the correct System.ComponentModel.Annotations assembly"
+                return [System.Reflection.Assembly]::LoadFrom($annotationsAssemblyPath)
+            }
+        }
+
+        # Register the event handler
+        [System.AppDomain]::CurrentDomain.add_AssemblyResolve($onAssemblyResolve)
     }
     else {
-        Write-Output("`nLoading Rubrik Security Cloud PowerShell Module (PowerShell Core)...`n")
+      # Write-Host "`nLoading Rubrik Security Cloud PowerShell Module (PowerShell Core)...`n"
         $moduleDir = Join-Path -Path $PSScriptRoot -ChildPath "net6.0"
     }
 
-    # To ensure that help files are loaded properly, we need to change directory to the 
-    # desired platform build (i.e. net742 or net6.0)
+    # Change to the directory containing the module
     Set-Location -Path $moduleDir
     $modulePath = Join-Path -Path $moduleDir -ChildPath "RubrikSecurityCloud.PowerShell.dll"
     
-    Write-Output "LoadModule.psm1: Import-Module $modulePath"
+    # Write-Host "LoadModule.psm1: Import-Module $modulePath"
     Import-Module $modulePath -ErrorAction Stop
 
-    Write-Output("Welcome to the Rubrik Security Cloud PowerShell SDK!`n")
-    Write-Output("`tPlease refer to the Getting Started section of the Readme documentation: ")
-    Write-Output("`thttps://github.com/rubrikinc/rubrik-powershell-sdk#getting-started`n")
+   # Write-Host "Welcome to the Rubrik Security Cloud PowerShell SDK!`n"
+   # Write-Host "`tPlease refer to the Getting Started section of the Readme documentation: "
+   # Write-Host "`thttps://github.com/rubrikinc/rubrik-powershell-sdk#getting-started`n"
 }
 catch {
-    Write-Error("Unable to load Rubrik Security Cloud Module: " + $_)
+    Write-Error "Unable to load Rubrik Security Cloud Module: $_"
 }
 finally {
     # Ensure the working directory has been set back to the original when the script exits
