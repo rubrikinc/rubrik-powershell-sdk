ba92ca1f70244884ec4d3a60511a783fb060e9c5
Gui <12204241+guirava@users.noreply.github.com>
2024-10-12 19:50:18 -0700

    util script update

--- Utils/Clean-RscSdk.ps1
+++ Utils/Clean-RscSdk.ps1
@@ -9,18 +9,18 @@ Set-Location $PSScriptRoot\..
 
 if (-Not $KeepOutputDir) {
     if (Test-Path -Path .\Output) {
-        Remove-Item -Recurse -Force .\Output
+        Remove-Item -Recurse -Force .\Output -ErrorAction Stop
     }
     if (Test-Path -Path .\Output.Release) {
-        Remove-Item -Recurse -Force .\Output.Release
+        Remove-Item -Recurse -Force .\Output.Release -ErrorAction Stop
     }
     if (Test-Path -Path .\Output.Publish) {
-        Remove-Item -Recurse -Force .\Output.Publish
+        Remove-Item -Recurse -Force .\Output.Publish -ErrorAction Stop
     }
 }
 Get-ChildItem -Path . -Recurse -Directory | Where-Object {
     $_.Name -ieq "obj" -or $_.Name -ieq "bin"
-} | Remove-Item -Force -Recurse
+} | Remove-Item -Force -Recurse -ErrorAction Stop
 Get-ChildItem -Path . -Recurse | Where-Object { 
     $_.Name -ieq "testResults.xml" -or $_.Name -ieq "tmp_help"
-} | Remove-Item -Force -Recurse
+} | Remove-Item -Force -Recurse -ErrorAction Stop
