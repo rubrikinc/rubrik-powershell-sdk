f3220d74681ff2f73a068d6e71ccb61d1169cf66
Gui <12204241+guirava@users.noreply.github.com>
2024-10-12 19:21:11 -0700

    util script update

--- Utils/admin/Update-RscSdkMainBranch.ps1
+++ Utils/admin/Update-RscSdkMainBranch.ps1
@@ -48,6 +48,10 @@ catch {
     throw "Failed to checkout 'main' branch."
 }
 
+# CHANGELOG.md can be a source of conflict so we check it out manually
+git checkout $sourceBranch -- CHANGELOG.md
+# Same with RubrikSecurityCloud.psd1
+git checkout $sourceBranch -- RubrikSecurityCloud\RubrikSecurityCloud.PowerShell\RubrikSecurityCloud.psd1
 # Merge $sourceBranch into main without commit
 try {
     git merge $sourceBranch --no-commit --no-ff
