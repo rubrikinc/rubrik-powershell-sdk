5d412d93811e595a3b8bd4b42e3dbd7e10669507
jakerobinson <jake.robinson@rubrik.com>
2025-01-02 15:33:35 -0500

    rewrite Get-RscMssqlLogShipping, add tests, table format

---
+++ Toolkit/Tests/e2e/Get-RscMssqlLogShipping.Tests.ps1
@@ -0,0 +1,40 @@
+BeforeAll {
+    . "$PSScriptRoot\..\E2eTestInit.ps1"
+
+    # variables shared among tests
+    $Global:data = @{
+        logShipping = $null
+    }
+}
+
+Describe -Name 'Get-RscMssqlLogShipping Tests' -Tag 'Public' -Fixture {
+
+    It -Name 'retrieves LogShippingTargets' -Test {
+        $data.logShipping = Get-RscMssqlLogShipping
+        # Write-Host "data.logShipping.count = $($data.logShipping.count)"
+        $data.logShipping | Should -Not -BeNullOrEmpty
+    }
+
+    Context -Name 'LogShippingTarget Count > 0' {
+        BeforeEach {
+            # Skip the tests if empty LogShippingTarget list
+            if ($data.logShipping.Count -le 0) {
+                Set-ItResult -Skipped -Because "At least 1 LogShippingTarget is needed"
+                return
+            }
+        }
+
+        It -Name 'retrieves single LogShippingTarget by Id' -Test {
+            $logShipping0 = Get-RscMssqlLogShipping -Id $data.logShipping[0].id
+            $logShipping0.id | Should -Be $data.logShipping[0].id
+        }
+
+        It -Name 'retrieves LogShippingTarget using filters' -Test {
+            $logShipping0 = Get-RscMssqlLogShipping -PrimaryDatabase (Get-RscMssqlDatabase -id $data.logShipping[0].primaryDatabase.Id) -SecondaryDatabaseName $data.logShipping[0].SecondaryDatabase.name
+            $logShipping0.id | Should -Be $data.logShipping[0].id
+        }
+        
+
+    }
+
+}
