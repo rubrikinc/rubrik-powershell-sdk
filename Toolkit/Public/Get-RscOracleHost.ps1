a7f0603679efe244cf2e0159c2d483e85eec83e1
Lokesh Kumar <lokesh.kumar@rubrik.com>
2025-03-13 00:48:10 -1200

    get-rscoraclehost fix

--- Toolkit/Public/Get-RscOracleHost.ps1
+++ Toolkit/Public/Get-RscOracleHost.ps1
@@ -30,11 +30,6 @@ function Get-RscOracleHost {
     Param(
         [Parameter(
             Mandatory = $false,
-            ParameterSetName = "Id"
-        )]
-        [String]$Id,
-        [Parameter(
-            Mandatory = $false,
             ParameterSetName = "Name"
         )]
         [String]$Name,
@@ -53,56 +48,40 @@ function Get-RscOracleHost {
     )
     
     Process {
+        $query = New-RscQuery -GqlQuery oracleTopLevelDescendants
+        $query.var.filter = @()
 
-       # The query is different for getting a single object by ID.
-        if ($Id) {
-            $query = New-RscQuery -GqlQuery 
-            $query.var.filter = @()
-            $query.Var.fid = $Id
-
-            $result = Invoke-Rsc -Query $query
-            $result
-        } else {
-            $query = New-RscQuery -GqlQuery oracleTopLevelDescendants
-            $query.var.filter = @()
-
-            if ($Name) {
-                $nameFilter = New-Object -TypeName RubrikSecurityCloud.Types.Filter
-                # Regex filter doesn't work in the API right now, but we're going to play pretend. 
-                # With real Regex, users could search for VMs that start with the letter A if they wanted.
-                if ($name.Contains("*")) {
-                    $name.Replace("*",'')
-                    $nameFilter.Field = [RubrikSecurityCloud.Types.HierarchyFilterField]::REGEX
-                    $nameFilter.texts = $Name.Replace("*",'')
-                } else {
-                    $nameFilter.Field = [RubrikSecurityCloud.Types.HierarchyFilterField]::NAME_EXACT_MATCH
-                    $nameFilter.texts = $Name
-                }
-                $query.var.filter += $nameFilter
+        if ($Name) {
+            $nameFilter = New-Object -TypeName RubrikSecurityCloud.Types.Filter
+            # Regex filter doesn't work in the API right now, but we're going to play pretend. 
+            # With real Regex, users could search for VMs that start with the letter A if they wanted.
+            if ($name.Contains("*")) {
+                $name.Replace("*",'')
+                $nameFilter.Field = [RubrikSecurityCloud.Types.HierarchyFilterField]::REGEX
+                $nameFilter.texts = $Name.Replace("*",'')
+            } else {
+                $nameFilter.Field = [RubrikSecurityCloud.Types.HierarchyFilterField]::NAME_EXACT_MATCH
+                $nameFilter.texts = $Name
             }
-    
-            if ($Sla) {
-                $slaFilter = New-Object -TypeName RubrikSecurityCloud.Types.Filter
-                $slaFilter.Field = [RubrikSecurityCloud.Types.HierarchyFilterField]::EFFECTIVE_SLA
-                $slaFilter.Texts = $Sla.id
-                $query.var.filter += $slaFilter
-            }
-
-            if ($Cluster) {
-                $clusterFilter = New-Object -TypeName RubrikSecurityCloud.Types.Filter
-                $clusterFilter.Field = [RubrikSecurityCloud.Types.HierarchyFilterField]::CLUSTER_ID
-                $clusterFilter.Texts = $Cluster.id
-                $query.var.filter += $clusterFilter
-            }
-
-            $result = Invoke-Rsc -Query $query
-            $result.nodes
+            $query.var.filter += $nameFilter
         }
 
+        if ($Sla) {
+            $slaFilter = New-Object -TypeName RubrikSecurityCloud.Types.Filter
+            $slaFilter.Field = [RubrikSecurityCloud.Types.HierarchyFilterField]::EFFECTIVE_SLA
+            $slaFilter.Texts = $Sla.id
+            $query.var.filter += $slaFilter
+        }
 
+        if ($Cluster) {
+            $clusterFilter = New-Object -TypeName RubrikSecurityCloud.Types.Filter
+            $clusterFilter.Field = [RubrikSecurityCloud.Types.HierarchyFilterField]::CLUSTER_ID
+            $clusterFilter.Texts = $Cluster.id
+            $query.var.filter += $clusterFilter
+        }
 
-
-
+        $result = Invoke-Rsc -Query $query
+        $result.nodes
     } 
 }
 
